ee0996d982002a0d1e0bcda5ccdcaa147ee7ac76
CasperHolt <jasper.holt@ucas-edu.net>
2025-01-31 11:36:30 -0700

    varibles py practice

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,4 +1,4 @@
-name=jasper
+name=input("what is your name:")
 
 number_1and10=6
 
@@ -10,3 +10,12 @@ color=input("what is your favorit color?: ")
 
 school="ucas"
 
+year="2025"
+
+eye=input("what color are your eyes:")
+
+age=input("how old are you:")
+
+subject=input("what is your favorit subject in school")
+
+print("it is", year,"and", name, "is", age, "has", eye,"eyes. goes to", school,"loves",subject,"loves the color", color, "and had", breakfast,"for breakfast today",number_1and10,number_1and100) 
