8eaa5428512077c7827c5572e542e6ce9cbbeca8
CasperHolt <jasper.holt@ucas-edu.net>
2025-01-31 11:15:04 -0700

    first program c

--- Template.c
+++ Template.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-float pi =3.14;
-int age = 15;
- char name[] = "Jasper";
 int main(void){
-    printf("helo i am%s. i am%d years old. i like the number \n", name);
-    prinf ("%f\n", pi);
-    printf("%d\n", age);
+   printf("hello world");
     return 0;
 }
