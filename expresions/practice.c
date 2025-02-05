eb2c757465e8a8b0c5724b4e0565a4f3c9c78732
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-05 11:48:28 -0700

    practic expresons c

---
+++ expresions/practice.c
@@ -0,0 +1,12 @@
+// Jasper Holt, TEMPLATE
+#include <stdio.h>
+#include <math.h>
+float expression1=7-24/8*4+6;
+float expression2=18/3-7+2*5;
+float expression3=6*4/12+72/8-9;
+int main(void){
+   printf("%f \n",expression1);
+   printf("%f \n",expression2);
+   printf
+    return 0;
+}
