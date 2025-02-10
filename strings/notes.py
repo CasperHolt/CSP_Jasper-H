40218de71b649019026f72ee0d1745c5d508a79a
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-10 11:49:05 -0700

    string note

---
+++ strings/notes.py
@@ -0,0 +1,9 @@
+#jasper holt, string notes .py
+
+#string is a data typ that holds info sorounded by " Quotaitin marks" '
+
+snetence="the quick brown fox jumps over the lazy dog"
+
+name=input("what is your name?\n").strip().lower().capitalize()
+print("hello",name)
+
