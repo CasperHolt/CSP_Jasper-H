fc86fcb04903f6ec3addf7a1ae497c30a100e1f0
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-11 11:15:24 -0700

    string notes

--- strings/notes.py
+++ strings/notes.py
@@ -2,8 +2,14 @@
 
 #string is a data typ that holds info sorounded by " Quotaitin marks" '
 
-snetence="the quick brown fox jumps over the lazy dog"
+snetence="the quick brown fox jumps over the lazy dog."
 
-name=input("what is your name?\n").strip().lower().capitalize()
-print("hello",name)
+#print(len(snetence))
+#print(snetence[4])
+satrt=snetence.find("brown")
+print(snetence[10:19])
+length=len("brown fox")
+
+#name=input("what is your name?\n").strip().lower().capitalize()
+#print("hello",name)
 
