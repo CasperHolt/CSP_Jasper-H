7a2ddbf72d8fa1e644aee1e57480d0b469c3dd02
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-12 11:28:07 -0700

    name decoraiteor py

---
+++ strings/name_deorator.py
@@ -0,0 +1,8 @@
+#jasper holt, name decoraitor .py
+
+name=input("what is your first name?\n").strip().lower().capitalize()
+
+string2='<---'
+string1="--->"
+result= string2 + name +string1
+print(result)
--- strings/notes.py
+++ strings/notes.py
@@ -6,10 +6,10 @@ snetence="the quick brown fox jumps over the lazy dog."
 
 #print(len(snetence))
 #print(snetence[4])
-satrt=snetence.find("brown")
-print(snetence[10:19])
-length=len("brown fox")
+#satrt=snetence.find("brown")
+#print(snetence[10:19])
+#length=len("brown fox")
 
-#name=input("what is your name?\n").strip().lower().capitalize()
-#print("hello",name)
+name=input("what is your name?\n").strip().lower().capitalize()
+print("hello",name)
 
