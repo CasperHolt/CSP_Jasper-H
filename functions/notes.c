c1792f83569259ac4d9ce9e8c9c6981cd8742c4e
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-18 11:49:08 -0700

    done

---
+++ functions/notes.c
@@ -0,0 +1,7 @@
+// Jasper Holt, functions notes c
+#include <stdio.h>
+
+int main(void){
+   printf("hello world");
+    return 0;
+}
---
+++ functions/notes.py
@@ -0,0 +1,11 @@
+# jasper holt, functions notes
+
+#functions hold actions
+number=int(input("pleas tell me a number:\n"))
+def add(numOne,numTwo):
+    
+    return (numOne+numTwo)
+
+print(add(number,12))
+addition=add(2,15)
+
