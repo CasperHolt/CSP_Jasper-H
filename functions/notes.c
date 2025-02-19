076bb53bd57f7b4591235724a7dfaffe88d5647e
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-19 11:30:21 -0700

    functions notes

--- functions/notes.c
+++ functions/notes.c
@@ -1,7 +1,17 @@
 // Jasper Holt, functions notes c
 #include <stdio.h>
 
-int main(void){
-   printf("hello world");
+
+void due(char assignment[50], char day[20]) {
+    printf("the %s assignment is due %s",assignment,day);
+
+    
+}
+
+int main(void) {
+    due("functions notes","today\n");
+    due("hello world update","tomorrow\n");
+    due("finachle calculator update","friday\n");
+
     return 0;
 }
