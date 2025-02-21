09870fe57965b2fa7231c6b76cfe7ad66cd9686d
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-21 11:24:40 -0700

    financhle calc update .py

--- functions/financhle_calc_update.py
+++ functions/financhle_calc_update.py
@@ -2,21 +2,21 @@
 
 def info(cost, income, type):
     percent= cost/income*100
-    print(f"your {type} is ${cost:.2f} wich is {percent}% of your income\n")
+    print(f"you spend ${cost:.2f} on {type} wich is {percent}% of your income\n")
+
+def ask(change):
+     return float(input(f"how much do you spend on {change} each month  \n"))
 
 #make a print statment that welcomes and tell the user what this program does
 print ("Hello there this is yuor financial calculator, pleas fill out the folowing inforaition and we will help you as best we can\n")
 #ask what income is (varible that should be an input)
 income = float(input("What is your income: \n"))
 
-#ask what their rent is (varible that should be an input)
-rent = float(input("how much rent do you pay each month: \n"))
-#ask what their utilities is (varible that should be an input)
-utilt= float(input("how much do you spend on utilities each month:\n"))
-#ask what their grosries is (varible that should be an input)
-groce=float(input("how much do you spend on groceries each month:\n"))
-#ask what their transportaition is (varible that should be an input)
-trans=float(input("how much do you spend on transportation;\n"))
+rent=ask("rent")
+utilt=ask("utilities")
+groce=ask("groceries")
+trans=ask("transportaitioin")
+
 #calculate savings as 10% of income ( income *.1) (variable)
 save = income *.1
 #calculate spending as income -savings-rent-utilites-grosreies-transport(varible)
@@ -26,6 +26,6 @@ spend= income - save - rent - utilt - groce- trans
 info(rent, income, "rent")
 info(utilt, income, "utilities")
 info(groce, income, "grocries")
-info(trans, income, "trans")
+info(trans, income, "transportaition")
 info(save, income, "savings")
 info(spend, income, "spending")
