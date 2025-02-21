aa7d5c081194ca3b8064dde119964fc597b3c371
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-21 16:33:28 -0700

    calc c update

--- functions/finachle_calc_update.c
+++ functions/finachle_calc_update.c
@@ -2,77 +2,51 @@
 #include <stdio.h>
 
 //ask what their rent is (varible that should be an input)
-//float income;
-//float rent;
- //loat utilt;
- //float groce;
-//float trans;
-//int main(void){
-   // printf ("Hello there this is your financial calculator, pleas fill out the folowing inforaition and we will help you as best we can\n");
-//}
-//int main(void){
+float income;
+float rent;
+float utilt;
+float groce;
+float trans;
+int main(void){
+    printf ("Hello there this is your financial calculator, pleas fill out the folowing inforaition and we will help you as best we can\n");
+}
+int main(void){
 
-//float save = income *.1;
+float save = income *.1;
 
 
-//float spend= income - save - rent - utilt - groce- trans;
+float spend= income - save - rent - utilt - groce- trans;
 
-//float perc_rent= rent/income*100;
+void pec(float cost[50],float income[50],char type[50]){
+    float percent= cost/income*100;
+    printf("you spend $%.2f on %s wich is %f%% of your income\n",cost,type,percent);
+    
+}
 
-//float perc_utilt= utilt/income*100;
-
-//float perc_groce= groce/income*100;
-
-//float perc_trans= trans/income*100;
-
-//float perc_spend= spend/income*100;
-
-//float perc_save= save/income*100;
-
-//printf ("your rent is $%.2f wich is %.2f %% of your income\n",rent,perc_rent);
-
-//printf ("your utilities are $%.2f wich is %.2f %% ofyour income\n",utilt,perc_utilt);
-
-//printf ("your groceries are $%.2f wich is %.2f %% of you income\n",groce,perc_groce);
-
-//printf ("you spend $%.2f on transportaition wich is %.2f %% of your income\n",trans,perc_trans);
-
-//printf ("you save $%.2f each moth wich is %.2f %% of your income\n",save,perc_save);
-
-//printf ("you spend $%.2f wich is %.2f %% of your income\n",spend,perc_spend);
 
  //ask what income is (varible that should be an input)
-    //return 0;
-//}
-float due(float var[20]) {
+    return 0;
+}
+float due(char var[50]) {
+    float value;
     printf("what is your %s\n",var);
-    scanf("%f", &var);  
-    
-        
+    scanf("%f", &value);  
+    return value;
     }
+
 int main(void){ 
 income=due("income");
 rent=due("rent");
 utilt=due("utilities");
 groce=due("groceries");
 trans=due("transportaition");
-}
-int main(void){
+
 
     float save = income *.1;
     
     
     float spend= income - save - rent - utilt - groce- trans;
     
-    float perc_rent= rent/income*100;
     
-    float perc_utilt= utilt/income*100;
-    
-    float perc_groce= groce/income*100;
-    
-    float perc_trans= trans/income*100;
-    
-    float perc_spend= spend/income*100;
-    
-    float perc_save= save/income*100;
+    return 0;
 }    
