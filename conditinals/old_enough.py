8cc5631048686dbf725fec8ba8107c360be87b02
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-26 11:49:12 -0700

    old enough

---
+++ conditinals/old_enough.py
@@ -0,0 +1,10 @@
+#Jasper holt, old enough py
+age=int(input(("How old are you\n")))
+if age>= 18:
+    print("you are old enough to vote\n")
+elif age >= 16:
+    print("you are old enough to drive")
+elif age==15:
+    print("you can get your learners permit")
+elif age>=6:
+    print("you can go to school")
