2947319898a31774e845a31a653fab37bed7dc36
CasperHolt <jasper.holt@ucas-edu.net>
2025-02-26 11:34:36 -0700

    finachel calc updt

--- functions/finachle_calc_update.c
+++ functions/finachle_calc_update.c
@@ -1,4 +1,4 @@
-// Jasper Holt, TEMPLATE
+// Jasper Holt, financhle calc update
 #include <stdio.h>
 
 //ask what their rent is (varible that should be an input)
@@ -7,46 +7,43 @@ float rent;
 float utilt;
 float groce;
 float trans;
-int main(void){
-    printf ("Hello there this is your financial calculator, pleas fill out the folowing inforaition and we will help you as best we can\n");
+
+int pec(float cost,float income,char type[]){
+    float percent = cost/income*100;
+    printf("you spend $%.2f on %s wich is %.2f%% of your income\n",cost,type,percent);
 }
-int main(void){
+float due(char var[]) {
+     float value;
+    printf("what is your %s\n",var);
+     scanf("%f", &value);  
+    return value;
 
-float save = income *.1;
-
-
-float spend= income - save - rent - utilt - groce- trans;
-
-void pec(float cost[50],float income[50],char type[50]){
-    float percent= cost/income*100;
-    printf("you spend $%.2f on %s wich is %f%% of your income\n",cost,type,percent);
     
-}
-
 
  //ask what income is (varible that should be an input)
     return 0;
 }
-float due(char var[50]) {
-    float value;
-    printf("what is your %s\n",var);
-    scanf("%f", &value);  
-    return value;
-    }
-
-int main(void){ 
-income=due("income");
-rent=due("rent");
-utilt=due("utilities");
-groce=due("groceries");
-trans=due("transportaition");
+int main(void){
+    printf ("Hello there this is your financial calculator, pleas fill out the folowing inforaition and we will help you as best we can\n");
 
 
+    income=due("income");
+    rent=due("rent");
+    utilt=due("utilities");
+    groce=due("groceries");
+    trans=due("transportaition");
     float save = income *.1;
     
     
     float spend= income - save - rent - utilt - groce- trans;
     
     
-    return 0;
-}    
+    
+pec(rent,income,"rent");
+pec(utilt,income,"utilities");
+pec(groce,income,"groceries");
+pec(trans,income,"transportaition");
+pec(save,income,"saveings");
+pec(spend,income,"spendings");
+return 0;
+}
