489c744f8ed4407867425b4799fa961788f69e57
CasperHolt <jasper.holt@ucas-edu.net>
2025-03-06 11:46:30 -0700

    first program c

---
+++ Variables/first program.c
@@ -0,0 +1,9 @@
+// Jasper Holt, first program
+#include <stdio.h>
+char name;
+int main(void){
+   printf("what is your name:");
+   scanf("%s",name);
+   printf("hello %s", name);
+    return 0;
+}
